1e69999433a8775063e0bf5637d99d08f6e5cc31
Sasundi Binara <sasubinara04@gmail.com>
2025-10-20 19:31:33 +0530

    Re Create Project

---
+++ DEA_Assignment/src/main/java/com/example/DEA_Assignment/AlHorizonApplication.java
@@ -0,0 +1,13 @@
+package com.example.DEA_Assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlHorizonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlHorizonApplication.class, args);
+	}
+
+}
---
+++ DEA_Assignment/src/test/java/com/example/DEA_Assignment/AlHorizonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DEA_Assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlHorizonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
