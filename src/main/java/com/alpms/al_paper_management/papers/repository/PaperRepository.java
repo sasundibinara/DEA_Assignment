1c4e563183f164229354565f72357afce4bbd19f
chalanima jayalakshi <jayalakshichala5@gmail.com>
2025-11-03 20:15:10 +0530

    edited PaperRepository.java

--- src/main/java/com/alpms/al_paper_management/papers/repository/PaperRepository.java
+++ src/main/java/com/alpms/al_paper_management/papers/repository/PaperRepository.java
@@ -1,7 +1,12 @@
 package com.alpms.al_paper_management.papers.repository;
 
 import com.alpms.al_paper_management.papers.model.Paper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PaperRepository extends JpaRepository<Paper, Long> {}
-
+public interface PaperRepository extends JpaRepository<Paper, Long> {
+    Page<Paper> findBySubjectId(Long subjectId, Pageable pageable);
+    Page<Paper> findByYear(Integer year, Pageable pageable);
+    Page<Paper> findBySubjectIdAndYear(Long subjectId, Integer year, Pageable pageable);
+}
