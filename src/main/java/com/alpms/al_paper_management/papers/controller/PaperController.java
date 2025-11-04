e015897049655c4a06d5fb84d5f7a7074493b783
chalanima jayalakshi <jayalakshichala5@gmail.com>
2025-11-05 00:28:54 +0530

    edited PaperController.java

--- src/main/java/com/alpms/al_paper_management/papers/controller/PaperController.java
+++ src/main/java/com/alpms/al_paper_management/papers/controller/PaperController.java
@@ -27,7 +27,7 @@ public class PaperController {
         this.subjectService = subjectService;
     }
 
-    // ✅ Single list endpoint (supports filters)
+    //  Single list endpoint (supports filters)
     @GetMapping
     public String list(
             @RequestParam(required = false) Long subjectId,
