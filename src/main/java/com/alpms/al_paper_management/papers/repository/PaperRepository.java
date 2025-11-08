7286928d88bec13296ee833e5e074188854b640b
dunalanjula <mdajanadara@students.nsbm.ac.lk>
2025-11-08 14:01:39 +0530

    Update PaperRepository.java

--- src/main/java/com/alpms/al_paper_management/papers/repository/PaperRepository.java
+++ src/main/java/com/alpms/al_paper_management/papers/repository/PaperRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PaperRepository extends JpaRepository<Paper, Long> {
     Page<Paper> findBySubjectId(Long subjectId, Pageable pageable);
     Page<Paper> findByYear(Integer year, Pageable pageable);
     Page<Paper> findBySubjectIdAndYear(Long subjectId, Integer year, Pageable pageable);
+    List<Paper> findBySubjectIdOrderByYearDesc(Long subjectId);
 }
